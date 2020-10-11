872e4ffd0c471abef42fbccf5edff83f6f317217
Bekhzod96 <mr.begzodjon96@mail.ru>
2020-10-11 23:27:21 +0300

    Configured with webPack

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // eslint-disable-next-line import/no-unresolved
 import React from 'react';
-import Images from './components/Images';
+import Images from './components/Images.jsx';
 function App() {
-  return (
-    <div>
-      <h3 className='heading'>Image Slider</h3>
-      <Images />
-    </div>
-  );
+	return (
+		<div>
+			<h3 className="heading">Image Slider</h3>
+			<Images />
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import './assets/css/index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
-
-serviceWorker.unregister();
